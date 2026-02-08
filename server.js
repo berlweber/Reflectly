b17bae3bc7a28f0674a748a966e9b9d272896661
berl weber <153825363+berlweber@users.noreply.github.com>
2026-02-08 11:41:24 +0100

    back-end boilerplate finished

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
+
+const port = process.env.port ? process.env.port : 3000;
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// routes here
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port: ${port}.`);
+});
