cf46e9f39b513c8c77741715b28972bd6da08211
polino <polinovskyoleksandr1303@gmail.com>
2026-02-09 13:11:56 +0100

    diary model

---
+++ controllers/auth.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../Reflectly/models/user');
+const jwt = require('jsonwebtoken');
+const { isPassportNumber } = require('validator');
+
+const saltRounds = 12;
+
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+            return res.status(409).json({error: 'Username already taken.'});
+        }
+
+        const displayNameInDatabase = await User.findOne({ displayName: req.body.displayName });
+        if (displayNameInDatabase) {
+            return res.status(409).json({error: 'Displayname already taken.'});
+        }
+
+        const user = await User.create({
+            username:req.body.username,
+            displayName: req.body.displayName,
+            email: req.body.email,
+            hashedPassword: bcrypt.hashSync(req.body.password, saltRounds),
+        });
+
+        const payload = { 
+            username: user.username,
+            _id: user._id,
+            displayName: user.displayName,
+            email: user.email
+        };
+
+        const token = jwt.sign({ payload }, process.env.JWT_SECRET);
+
+        res.status(201).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/sign-in', async (req, res)  => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials.' });
+        };
+
+        const isPassportCorrect = bcrypt.compareSync(req.body.password, user.hashedPassword);
+        if (!isPassportCorrect){
+            return res.status(401).json({ error: 'Invalid credentials.' });
+        };
+
+        const payload = { 
+            username: user.username,
+            _id: user._id,
+            displayName: user.displayName,
+            email: user.email
+        };
+
+        const token = jwt.sign({ payload }, process.env.JWT_SECRET);
+        
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
