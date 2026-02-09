a2c45f19c226e40819c8a473fe50a6482397b77e
polinovskyyoleksandr <polinovskyoleksandr1303@gmail.com>
2026-02-09 16:00:29 +0100

    added the import for verifytoken

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router(); 
 const Comment = require('../models/comment'); 
 const jwt = require('jsonwebtoken');
+const verifyToken = require('../middleware/verify-token')
 
 // create a new comment
 // method is POST
